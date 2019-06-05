ad90a117d81400832abfab69a6f82187e2b16c29
Tim Flink <tflink@fedoraproject.org>
2019-06-05 14:02:11 -0600

    first attempt at jenkins master config

---
+++ config/s2i/jenkins/master/configuration/init.groovy
@@ -0,0 +1,49 @@
+import java.util.logging.Logger
+import jenkins.security.s2m.*
+import hudson.model.*
+import hudson.security.*
+import jenkins.model.*
+import com.cloudbees.plugins.credentials.*
+import com.cloudbees.plugins.credentials.common.*
+import com.cloudbees.plugins.credentials.domains.*
+import com.cloudbees.jenkins.plugins.sshcredentials.impl.*
+import hudson.slaves.*
+import hudson.slaves.EnvironmentVariablesNodeProperty.Entry
+import hudson.plugins.sshslaves.*
+import hudson.plugins.sshslaves.verifiers.*
+import hudson.plugins.openid.*
+
+def logger = Logger.getLogger("")
+logger.info("Disabling CLI over remoting")
+jenkins.CLI.get().setEnabled(false);
+logger.info("Enable Slave -> Master Access Control")
+Jenkins.instance.injector.getInstance(AdminWhitelistRule.class).setMasterKillSwitch(false)
+Jenkins.instance.save()
+
+global_domain = Domain.global()
+
+// setup OAuth against id.fp.o
+def loginRealm = new OpenIdSsoSecurityRealm("https://id.fedoraproject.org/")
+Jenkins.instance.setSecurityRealm(loginRealm)
+
+def matrix_auth = new ProjectMatrixAuthorizationStrategy()
+matrix_auth.add(hudson.model.Hudson.READ,'anonymous')
+matrix_auth.add(hudson.model.Item.DISCOVER,'anonymous')
+matrix_auth.add(hudson.model.Item.READ,'anonymous')
+matrix_auth.add(hudson.model.Hudson.ADMINISTER, 'sysadmin-qa')
+
+Jenkins.instance.setAuthorizationStrategy(matrix_auth)
+
+// set up fedmsg providers
+logger.info("Setup fedora-fedmsg Messaging Provider")
+FedMsgMessagingProvider fedmsg = new FedMsgMessagingProvider("fedora-fedmsg", "tcp://hub.fedoraproject.org:9940", "tcp://172.19.4.24:9941", "org.fedoraproject");
+GlobalCIConfiguration.get().addMessageProvider(fedmsg)
+
+logger.info("Setup fedora-fedmsg-stage Messaging Provider")
+FedMsgMessagingProvider fedmsgStage = new FedMsgMessagingProvider("fedora-fedmsg-stage", "tcp://stg.fedoraproject.org:9940", "tcp://172.19.4.36:9941", "org.fedoraproject");
+GlobalCIConfiguration.get().addMessageProvider(fedmsgStage)
+
+logger.info("Setup fedora-fedmsg-devel Messaging Provider")
+FedMsgMessagingProvider fedmsgDevel = new FedMsgMessagingProvider("fedora-fedmsg-devel", "tcp://fedmsg-relay.continuous-infra.svc:4001", "tcp://fedmsg-relay.continuous-infra.svc:2003", "org.fedoraproject");
+GlobalCIConfiguration.get().addMessageProvider(fedmsgDevel)
+
