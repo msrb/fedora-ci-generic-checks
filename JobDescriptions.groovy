aa42b7ae9406be1986d3680a22d34d64b8db90b5
Tim Flink <tflink@fedoraproject.org>
2019-06-06 09:30:44 -0600

    add CANNED_CI_MESSAGE so we're not just waiting for builds to happen

--- JobDescriptions.groovy
+++ JobDescriptions.groovy
@@ -1,6 +1,9 @@
 pipelineJob('Fedora Build Check'){
     description 'Job to run checks on Fedora builds'
 
+    // default so we don't need to wait around for builds to happen
+    def CANNED_CI_MESSAGE = '{"commit":{"username":"eseyman","stats":{"files":{"perl-Net-FTPSSL.spec":{"deletions":2,"additions":5,"lines":7},".gitignore":{"deletions":0,"additions":1,"lines":1},"sources":{"deletions":1,"additions":1,"lines":2}},"total":{"deletions":3,"files":3,"additions":7,"lines":10}},"name":"Emmanuel Seyman","rev":"c1c7de158fa72de5bd279daaaac9f75d0b3e65cd","namespace":"rpms","agent":"eseyman","summary":"Update to 0.40","repo":"perl-Net-FTPSSL","branch":"master","seen":false,"path":"/srv/git/repositories/rpms/perl-Net-FTPSSL.git","message":"Update to 0.40\n","email":"emmanuel@seyman.fr"},"topic":"org.fedoraproject.prod.git.receive"}'
+
     // Audit file for all messages sent.
     msgAuditFile = "messages/message-audit.json"
 
@@ -39,7 +42,7 @@ pipelineJob('Fedora Build Check'){
         }
     }
     parameters{
-        stringParam('CI_MESSAGE', '{}', 'fedora-fedmsg')
+        stringParam('CI_MESSAGE', CANNED_CI_MESSAGE, 'fedora-fedmsg')
     }
 
 
