3f8eb95656d8cc5159ab4a76e6d7a74aec800d03
Tim Flink <tflink@fedoraproject.org>
2019-06-17 17:13:30 +0200

    fix canned message to work for builds

--- JobDescriptions.groovy
+++ JobDescriptions.groovy
@@ -2,7 +2,7 @@ pipelineJob('fedora-build-check'){
     description 'Job to run checks on Fedora builds'
 
     // default so we don't need to wait around for builds to happen
-    def CANNED_CI_MESSAGE = '{"commit":{"username":"eseyman","stats":{"files":{"perl-Net-FTPSSL.spec":{"deletions":2,"additions":5,"lines":7},".gitignore":{"deletions":0,"additions":1,"lines":1},"sources":{"deletions":1,"additions":1,"lines":2}},"total":{"deletions":3,"files":3,"additions":7,"lines":10}},"name":"Emmanuel Seyman","rev":"c1c7de158fa72de5bd279daaaac9f75d0b3e65cd","namespace":"rpms","agent":"eseyman","summary":"Update to 0.40","repo":"perl-Net-FTPSSL","branch":"master","seen":false,"path":"/srv/git/repositories/rpms/perl-Net-FTPSSL.git","message":"Update to 0.40\n","email":"emmanuel@seyman.fr"},"topic":"org.fedoraproject.prod.git.receive"}'
+    def CANNED_CI_MESSAGE = '{"build_id":1288823,"old":0,"name":"sssd","task_id":35594360,"attribute":"state","request":["git+https://src.fedoraproject.org/rpms/sssd.git#80b558654cf4cbb72c267b82ff9395cb531dab93","f30-candidate",{}],"instance":"primary","epoch":null,"version":"2.2.0","owner":"mzidek","new":1,"release":"1.fc30"}'
 
     // Audit file for all messages sent.
     msgAuditFile = "messages/message-audit.json"
