61a4b124e16de4ec471d221eaa684dfccf6db272
Tim Flink <tflink@fedoraproject.org>
2019-07-10 10:49:40 -0600

    first attempt to get message emitting to work

--- RpmInspectBasic.groovy
+++ RpmInspectBasic.groovy
@@ -44,6 +44,94 @@ def executeInContainer(String stageName,
         """
     }
 }
+
+/**
+ * Library to set message fields to be published
+ * @param messageType: ${MAIN_TOPIC}.ci.pipeline.allpackages.<defined-in-README>
+ * @param artifact ${MAIN_TOPIC}.ci.pipeline.allpackages-${artifact}.<defined-in-README>
+ * @return
+ */
+def setMessageFields(String messageType, String artifact) {
+    topic = "${MAIN_TOPIC}.ci.pipeline.allpackages-${artifact}.${messageType}"
+    print("Topic is " + topic)
+
+    // Create a HashMap of default message content keys and values
+    // These properties should be applicable to ALL message types.
+    // If something is applicable to only some subset of messages,
+    // add it below per the existing examples.
+
+    taskid = env.fed_task_id ?: env.fed_id
+
+    def messageContent = [
+            branch           : env.fed_branch,
+            build_id         : env.BUILD_ID,
+            build_url        : env.JENKINS_URL + 'blue/organizations/jenkins/' + env.JOB_NAME + '/detail/' + env.JOB_NAME + '/' + env.BUILD_NUMBER + '/pipeline/',
+            namespace        : env.fed_namespace,
+            nvr              : env.nvr,
+            original_spec_nvr: env.original_spec_nvr,
+            ci_topic         : topic,
+            ref              : env.basearch,
+            scratch          : env.isScratch ? env.isScratch.toBoolean() : "",
+            repo             : env.fed_repo,
+            rev              : (artifact == 'build') ? "kojitask-" + taskid : env.fed_rev,
+            status           : currentBuild.currentResult,
+            test_guidance    : "''",
+            comment_id       : env.fed_lastcid,
+            username         : env.fed_owner,
+    ]
+
+    if (artifact == 'pr') {
+        messageContent.commit_hash = env.fed_last_commit_hash
+    }
+
+    // Add image type to appropriate message types
+    if (messageType in ['image.queued', 'image.running', 'image.complete', 'image.test.smoke.queued', 'image.test.smoke.running', 'image.test.smoke.complete'
+    ]) {
+        messageContent.type = messageType == 'image.running' ? "''" : 'qcow2'
+    }
+
+    // Create a string to hold the data from the messageContent hash map
+    String messageContentString = JsonOutput.toJson(messageContent)
+
+    def messagePropertiesString = ''
+
+    return [ 'topic': topic, 'properties': messagePropertiesString, 'content': messageContentString ]
+}
+
+/**
+ * Library to send message
+ * @param msgProps - Message Properties - empty string for fedmsg
+ * @param msgContent - Message content in map form
+ * @return
+ */
+def sendMessage(String msgTopic, String msgProps, String msgContent) {
+
+    retry(10) {
+        try {
+            // 1 minute should be more than enough time to send the topic msg
+            timeout(1) {
+                try {
+                    // Send message and return SendResult
+                    sendResult = sendCIMessage messageContent: msgContent,
+                            messageProperties: msgProps,
+                            messageType: 'Custom',
+                            overrides: [topic: msgTopic],
+                            failOnError: true,
+                            providerName: "${MSG_PROVIDER}"
+                    return sendResult
+                } catch(e) {
+                    throw e
+                }
+            }
+        } catch(e) {
+            echo "FAIL: Could not send message to ${MSG_PROVIDER}"
+            echo e.getMessage()
+            sleep 30
+            error e.getMessage()
+        }
+    }
+}
+
     // Execution ID for this run of the pipeline
     def executionID = UUID.randomUUID().toString()
     def podName = 'fedora-package-check-' + executionID
@@ -86,28 +174,29 @@ node(podName) {
 
     currentStage = "run-rpminspect"
     stage(currentStage) {
+        env.MSG_PROVIDER = "fedora-fedmsg-stage"
         def json_message = readJSON text: env.CI_MESSAGE
         env.TARGET_ENVR = "${json_message['name']}-${json_message['version']}-${json_message['release']}"
 
         // Set our message topic, properties, and content
-        //messageFields = packagepipelineUtils.setMessageFields("package.test.functional.queued", env.TARGET_ENVR)
+        messageFields = setMessageFields("package.test.functional.queued", env.TARGET_ENVR)
 
         // Send message org.centos.prod.ci.pipeline.allpackages.package.test.functional.queued on fedmsg
-        //pipelineUtils.sendMessage(messageFields['topic'], messageFields['properties'], messageFields['content'])
+        pipelineUtils.sendMessage(messageFields['topic'], messageFields['properties'], messageFields['content'])
 
         // Set stage specific vars
         //packagepipelineUtils.setStageEnvVars(currentStage)
 
         // Set our message topic, properties, and content
-        //messageFields = packagepipelineUtils.setMessageFields("package.test.functional.running", env.TARGET_ENVR)
+        messageFields = setMessageFields("package.test.functional.running", env.TARGET_ENVR)
 
         executeInContainer(currentStage, "package-checks", "/tmp/run-rpminspect.sh")
 
         // Set our message topic, properties, and content
-        //messageFields = packagepipelineUtils.setMessageFields("package.test.functional.complete", env.TARGET_ENVR)
+        messageFields = setMessageFields("package.test.functional.complete", env.TARGET_ENVR)
 
         // Send message org.centos.prod.ci.pipeline.allpackages.package.test.functional.complete on fedmsg
-        //pipelineUtils.sendMessage(messageFields['topic'], messageFields['properties'], messageFields['content'])
+        pipelineUtils.sendMessage(messageFields['topic'], messageFields['properties'], messageFields['content'])
 
     }
     stage("archive output"){
