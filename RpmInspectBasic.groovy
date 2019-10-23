377814d4d68f919495f770cfaafc7263d44fe280
Tim Flink <tflink@fedoraproject.org>
2019-10-23 12:33:02 -0600

    get rid of jobdsl classpath stuff, add global shared library to actual job

--- RpmInspectBasic.groovy
+++ RpmInspectBasic.groovy
@@ -2,14 +2,16 @@
 
 import groovy.json.JsonOutput
 
-library identifier: "build-check@develop",
-        retriever: modernSCM([$class: 'GitSCMSource',
-                              remote: "https://pagure.io/fedora-ci-generic-checks.git",
-                              traits: [[$class: 'jenkins.plugins.git.traits.BranchDiscoveryTrait'],
-                                       [$class: 'RefSpecsSCMSourceTrait',
-                                        templates: [[value: '+refs/heads/*:refs/remotes/@{remote}/*'],
-                                                    [value: '+refs/pull/*:refs/remotes/origin/pr/*']]]]])
+//library identifier: "build-check@develop",
+//        retriever: modernSCM([$class: 'GitSCMSource',
+//                              remote: "https://pagure.io/fedora-ci-generic-checks.git",
+//                              traits: [[$class: 'jenkins.plugins.git.traits.BranchDiscoveryTrait'],
+//                                       [$class: 'RefSpecsSCMSourceTrait',
+//                                        templates: [[value: '+refs/heads/*:refs/remotes/@{remote}/*'],
+//                                                    [value: '+refs/pull/*:refs/remotes/origin/pr/*']]]]])
 
+// load all parts of buildCheckUtils, global library defined in jenkins config
+@Library('buildCheckUtils') _
 
 /**
  * Function to execute script in container
